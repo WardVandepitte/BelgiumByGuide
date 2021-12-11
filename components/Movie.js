02f71510eb068cb317bdb0fe20234c3cd1e8b320
mdquesne <maximilien.dequesne@gmail.com>
2021-12-11 10:07:58 +0100

    directors lijn 44

--- components/Movie.js
+++ components/Movie.js
@@ -41,7 +41,7 @@ const Movie = ({ data, level }) => {
     locale = data.story.lang;
     var content = data.story.content;
     var directors = data.rels.filter(obj => {
-      return content.Directors.includes(obj.uuid);
+      return content.directors.includes(obj.uuid);
     });
     var stars = data.rels.filter(obj => {
       return content.stars.includes(obj.uuid);
