1b03cf443764949dfd57af20ca23a8e6304f7679
mdquesne <you@example.com>
2021-12-21 20:29:17 +0100

    ok

--- components/EmailOptin.js
+++ components/EmailOptin.js
@@ -10,7 +10,7 @@ const EmailOptin = ({ }) => {
     
     <div className={styles.emailoptin}>
       <div className={styles.optinbutton} onClick={() => toggleformstate()}>Keep me posted through the newsletter</div>
-      {/* {formstate&&<iframe width="540" height="900" src="" frameborder="0" scrolling="auto" allowfullscreen style={{ display: `block`, marginLeft: `auto`, marginRight: `auto`, maxWidth: '100%' }}></iframe>} */}
+      {formstate&&<iframe width="540" height="305" src="https://4cd09f9c.sibforms.com/serve/MUIEADxcIVYOIpMd36YPUaS6k2EBaRTji49JBPO0fpEht7fMx2U8Z4Cmt9602Rrmu_ZVhj4UKPad4E-1UWWrFOEh3TKgiB3oBAjQbIu5Uzwlo7wBFL0HG7kHbTqiWlPt7gWT1LNrD1cW8a46B2r4AkpPKZRy85ypgIP3NSRcCFBBZOfLAnximooGVpoMns7_2ZBg2QWste6KecRp" frameborder="0" scrolling="auto" allowfullscreen style="display: block;margin-left: auto;margin-right: auto;max-width: 100%;"></iframe>}
     </div>
   );
 };
