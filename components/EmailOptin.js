c988661dacb1fb5f29b3166a331f52c6e7f57190
mdquesne <you@example.com>
2021-12-21 21:24:02 +0100

    lol

--- components/EmailOptin.js
+++ components/EmailOptin.js
@@ -10,7 +10,8 @@ const EmailOptin = ({ }) => {
     
     <div className={styles.emailoptin}>
       <div className={styles.optinbutton} onClick={() => toggleformstate()}>Keep me posted through the newsletter</div>
-      {formstate&&<iframe width="540" height="305" src="https://4cd09f9c.sibforms.com/serve/MUIEAEtYTE8NKM6Jkugd162D6W3iRJ2jjktsp0NCM0fCDBTgW0oyG5JTiLlzpT_PzSHYGQAyuxMfx1Ave2zyrQECj0z7b9NqHm_O_YQmsR3plD1lHIzy5wrTt7cyIHEZH1xb-7LRkHmdDJl4wP2-OKAYk6GQ5Dca_6nkTXfWt6NMthVFEEC-3odZXo_GyNExyGscIVAJ0GZvX5nX" frameborder="0" scrolling="auto" allowfullscreen style="display: block;margin-left: auto;margin-right: auto;max-width: 100%;"></iframe>}
+      {formstate&&<<iframe width="540" height="305" src="https://4cd09f9c.sibforms.com/serve/MUIEAPseK0L1TdyJ3mBj4JURkT_XfypAnAPxrk7ApbxbnjRgknuxb70etEGve5Fcj4zBUlZhXGOE4mOaf4gmJT8Ngby5EnBFzmDVipeRW_3ttTUiyKFUdrC-fofHdRWNlDEb-oj0t1XYw4BNIwNde--uV4WU670bXW-S4uvrVaI-7YjHHzS-ZGiInBcX28OSWg6XF4u6y8wLQN9F" frameborder="0" scrolling="auto" allowfullscreen style="display: block;margin-left: auto;margin-right: auto;max-width: 100%;"></iframe>}
+      
     </div>
   );
 };
